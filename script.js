ad0176ba6a67a2fd8c105b5c5336548ff1260d54
AsherCode <alonsosandovalch@gmail.com>
2023-03-18 20:34:27 -0500

    add basic minimax algorithm

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const user = "X";
 const computer = "O";
 let currentPlayer = user;
 let running = false;
+let winner = '';
 
 restartBtn.addEventListener('click', restartGame);
 
@@ -24,10 +25,11 @@ restartBtn.addEventListener('click', restartGame);
 initGame();
 
 function initGame(){
-    cells.forEach(cell => {
-        cell.textContent="";
-        cell.addEventListener('click', cellClicked)
-    });
+    for(let i = 0;i < cells.length; i++){
+        cells[i].textContent = options[i];
+        cells[i].addEventListener('click', cellClicked);
+    }
+
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 }
@@ -43,21 +45,33 @@ function cellClicked(){
 }
 
 function updateCell(cell, index){
+    console.log('updateCell called')
     options[index] = currentPlayer;
+    // console.log("options: ", options);
+    // console.log("index: ", index);
+
     cell.textContent = currentPlayer;
 }
 
 function changePlayer(){
+    console.log('change player called')
     if (currentPlayer === user) {
+        //computer's turn
         currentPlayer = computer;
+        aiPlayer(options);
     } else {
+
         currentPlayer = user;
     }
+
+
     statusText.textContent = `${currentPlayer}'s turn`;
 }
 
 function checkWinner(){
+    console.log('checkwinner called')
     let roundWon = false;
+
     for(const condition of winConditions){
         const sliceCells = [
             options[condition[0]],
@@ -74,17 +88,22 @@ function checkWinner(){
             break;
         }
     }
+    console.log('roundWon:', roundWon);
 
-    if(roundWon){
-        statusText.textContent = `${currentPlayer} wins!!`
+if(roundWon){
+        winner = currentPlayer;
+        minimax();
+        statusText.textContent = `${winner} wins!!`;
         running = false;
-        return 'X';
     } else if(!options.includes("")){
+        winner = 'tie';
+        minimax();
         statusText.textContent = `It's a tie!`
-        return 'tie'
     } else {
         changePlayer();
     }
+
+
 }
 
 function restartGame(){
@@ -92,3 +111,107 @@ function restartGame(){
     options = Array.from({length: 9}, (_) => "");
     initGame();
 }
+
+const score = {
+    O: 1,
+    X: -1,
+    tie: 0
+};
+
+function aiPlayer(board){
+    console.log('aiplayer called')
+    let bestScore = -Infinity;
+    let bestMove;
+
+    for(let i = 0; i < board.length; i++) {
+        if(board[i] === '') {
+            board[i] = computer;
+            
+            let localScore = minimax(board, 0, true);
+            console.log(`localScore:`, localScore)
+
+            //reset
+            board[i] = "";
+            console.log(`localMove:`, i);
+            
+            if(localScore > bestScore){
+
+                bestScore = localScore;
+                bestMove = i;
+            }
+        }
+    }
+
+    board[bestMove] = computer;
+    updateCell(cells[bestMove], bestMove);
+    setTimeout(() => {
+        checkWinner();
+    }, 0);
+
+}
+
+function custom(board){
+    console.log('custom called')
+    console.log(`winner: `, winner);
+
+    if(winner !== '') {
+       return score[winner];
+    }
+
+    return 1;
+}
+
+
+function minimax(board, depth, ismax){
+    console.log(depth, ismax ? 'MAXIMIZING' : 'MINIMIZING', 'BOARD:', board);
+
+
+    if(winner !== '') {
+        console.log(`current winner:`, score[winner]);
+       return score[winner];
+    } else if(!board.includes("")){
+        return 0;
+    }
+
+
+
+
+    if(ismax){
+        let maxScore = -Infinity;
+
+        for(let i=0; i < board.length; i++) {
+            if(board[i] === '') {
+                board[i] = computer;
+                let localScore = minimax([...board], depth + 1, false);
+                
+                //reset
+                board[i] = "";
+                
+                if(localScore > maxScore){
+                    maxScore = localScore;
+                }
+            } 
+        }
+        console.log(depth, 'RETURNING FROM MAXIMIZING');
+        return maxScore;
+        
+    } else {
+        let minScore = Infinity;
+
+        for(let i=0; i < board.length; i++) {
+            if(board[i] === '') {
+                board[i] = user;
+                let localScore = minimax([...board], depth + 1, true);
+                
+                //reset
+                board[i] = "";
+                if(localScore < minScore){
+                    minScore = localScore;
+                }
+            }
+        }
+        console.log(depth, 'RETURNING FROM MAXIMIZING');
+        return minScore;
+    }
+}
+
